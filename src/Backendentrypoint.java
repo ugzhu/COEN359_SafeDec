0e0a383048c78bbe9ceb76c31aab5b780265c933
Yujie Zhu <yzhu@Yujies-MacBook-Pro.local>
2022-11-13 19:50:33 -0800

    updated infomation structure passed from ui to backend

--- src/Backendentrypoint.java
+++ src/Backendentrypoint.java
@@ -4,44 +4,33 @@ import java.util.List;
 
 public class Backendentrypoint {
 
-    public void EntryMainfunc (ArrayList<HomeRoomCreator> roomlist, User userinput) {
+    public void EntryMainfunc (List<HashMap<String, Object>> spec, User user) {
         System.out.println("********Backend Execution started");
 
-        User userobj = userinput;
-        //dummy values passed from GUI
-        List<HashMap<String, Object>> layout = new ArrayList<>();
-        HashMap<String, Object> room1 = new HashMap<>();
-        HashMap<String, Object> room2 = new HashMap<>();
-        HashMap<String, Object> room3 = new HashMap<>();
-        String startTime;
-        String endTime;
-
-        room1.put("id", 1);
-        room1.put("fire", true);
-        room1.put("security", true);
-        room1.put("camera", true);
-
-        room2.put("id", 2);
-        room2.put("fire", true);
-        room2.put("security", false);
-        room2.put("camera", true);
-
-        room3.put("id", 3);
-        room3.put("fire", false);
-        room3.put("security", true);
-        room3.put("camera", false);
-
-        layout.add(room1);
-        layout.add(room2);
-        layout.add(room3);
-
-        startTime = "00:00:00";
-        endTime = "23:59:59";
 
 
-        // dummy layout looks like: [{security=true, fire=true, id=1, camera=true, schedule obj1},
-        //                           {security=false, fire=true, id=2, camera=true, schedule obj2},
-        //                           {security=true, fire=false, id=3, camera=false, schedule obj3}]
+
+        // dummy spec looks like: [{"security"=true, "fire"=true, "id"=1, "camera"=true, "startTime"="01:11:22", "endTime"="16:42:59"},
+        //                           {"security"=false, "fire"=true, "id"=2, "camera"=true, "startTime"="01:11:22", "endTime"="16:42:59"},
+        //                           {"security"=true, "fire"=false, "id"=3, "camera"=false, "startTime"="01:11:22", "endTime"="16:42:59"}]
+
+
+
+//        HashMap<String, Object> rm1 = new HashMap<>();
+//        rm1.put("security", true);
+//        rm1.put("fire", true);
+//        rm1.put("camera", true);
+//        rm1.put("id", 1);
+//        rm1.put("startTime", "01:11:22");
+//        rm1.put("endTime", "16:42:59");
+//        HashMap<String, Object> rm2 = new HashMap<>();
+//        HashMap<String, Object> rm3 = new HashMap<>();
+//
+//        List<HashMap<String, Object>> spec = new ArrayList<>();
+//        info.add(rm1);
+//        info.add(rm2);
+//        info.add(rm3);
+
 
 
 
@@ -55,20 +44,14 @@ public class Backendentrypoint {
         /// create all rooms with/without services and camera
         List<Room> rooms = new ArrayList<>();
         RoomBuilder roomBuilder = HomeRoomBuilder.instance();
-        for (HashMap<String, Object> stringObjectHashMap : layout) {
+        for (HashMap<String, Object> stringObjectHashMap : spec) {
             Room r = roomBuilder.buildRoom(stringObjectHashMap);
             rooms.add(r);
         }
 
 
-
-        // create user
-        String username = "my_username";
-        String password = "my_pwd";
-        String phone = "my_phone";
-        User user = new HomeUser(username, password, phone);
-
-        // data generated: List<Room> rooms, User user
+        // data generated: List<Room> rooms // 3 rooms in total
+        // Room room = rooms.get(0)
 
         // check fire service: room.hasFireService -> boolean
         // check security service: room.hasSecurityService -> boolean
@@ -76,9 +59,6 @@ public class Backendentrypoint {
         // get id: room.getId() -> int
         // get duration room.getSchedule.getDuration() -> long
 
-        // get password: user.getPassword() -> String
-        // get username: user.getUsername() -> String
-        // get phone: user.getPhone() -> String
 
         // Covered - simuated in GUI. Will review , so that we dont need it here.
         //////// demo break in////
