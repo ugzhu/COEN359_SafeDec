d276bb6a2ad603bc8bb25d039a2f0ababdfa997e
Himani Vaidya <hvaidya@scu.edu>
2022-11-19 10:55:30 -0800

    review changes

--- src/Backendentrypoint.java
+++ src/Backendentrypoint.java
@@ -8,54 +8,13 @@ public class Backendentrypoint {
         System.out.println("********Backend Execution started");
 
         User userobj = userinput;
-        //dummy values passed from GUI
-        List<HashMap> layout = new ArrayList<>();
-        HashMap<String, Object> room1 = new HashMap<>();
-        HashMap<String, Object> room2 = new HashMap<>();
-        HashMap<String, Object> room3 = new HashMap<>();
-        String startTime;
-        String endTime;
-
-        room1.put("id", 1);
-        room1.put("fire", true);
-        room1.put("security", true);
-        room1.put("camera", true);
-
-        room2.put("id", 2);
-        room2.put("fire", true);
-        room2.put("security", false);
-        room2.put("camera", true);
-
-        room3.put("id", 3);
-        room3.put("fire", false);
-        room3.put("security", true);
-        room3.put("camera", false);
-
-        layout.add(room1);
-        layout.add(room2);
-        layout.add(room3);
-
-        startTime = "00:00:00";
-        endTime = "23:59:59";
-
-        // Data from GUI
-        // dummy layout looks like: [{security=true, fire=true, id=1, camera=true, schedule obj1},
-        //                           {security=false, fire=true, id=2, camera=true, schedule obj2},
-        //                           {security=true, fire=false, id=3, camera=false, schedule obj3}]
-
-
-        // value passed from GUI: layout, schedule object
-
-
-        //////////////////////////////////
-        ///////    Yujie      ///////////
-        /////////////////////////////////
 
         /// create all rooms with/without services and camera
         List<Room> rooms = new ArrayList<>();
         RoomBuilder roomBuilder = HomeRoomBuilder.instance();
-        for (int i = 0; i < layout.size(); i++) {
-            Room r = roomBuilder.buildRoom(layout.get(i));
+        for (int i = 0; i < roomlist.size(); i++) {
+            Room r = roomBuilder.buildRoom(roomlist.get(i));
+            System.out.println("start time is "+roomlist.get(i).get("startTime"));
             rooms.add(r);
         }
 
